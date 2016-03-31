a41a602f63cd858d4db555a3a365633113c6411f
Bourgond Aries <macocio@gmail.com>
2016-03-31 19:32:51 +0200

    Add diesel to the mix

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,46 @@
-#![feature(plugin)]
-#![plugin(maud_macros)]
+#![feature(custom_derive, custom_attribute, plugin)]
+#![plugin(diesel_codegen, dotenv_macros, maud_macros)]
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
 extern crate iron;
 extern crate maud;
 extern crate router;
 
+mod models;
+mod schema;
+
+use self::models::*;
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use dotenv::dotenv;
 use iron::mime::*;
 use iron::prelude::*;
 use iron::status;
 use router::Router;
+use std::env;
+
+pub fn create_post<'a>(conn: &PgConnection, title: &'a str, body: &'a str) -> Post {
+	use schema::posts;
+
+	let new_post = NewPost {
+		title: title,
+		body: body,
+	};
+
+	diesel::insert(&new_post).into(posts::table)
+		.get_result(conn)
+		.expect("Can't save new post")
+}
+
+pub fn establish_connection() -> PgConnection {
+	dotenv().ok();
+
+	let database_url = env::var("DATABASE_URL")
+		.expect("DATABASE_URL must be set in the .env");
+	PgConnection::establish(&database_url)
+		.expect(&format!("Error connecting to {}", database_url))
+}
 
 fn setup_router() -> Router {
 	let mut router = Router::new();
@@ -17,6 +50,18 @@ fn setup_router() -> Router {
 }
 
 fn main() {
+	use schema::posts::dsl::*;
+
+	let con = establish_connection();
+
+	create_post(&con, "Hello world", "Today is a good day!");
+
+	let res = posts
+		.limit(5)
+		.load::<Post>(&con)
+		.expect("Could not load posts");
+
+	println!("Posts: {}", res.len());
 
 	let router = setup_router();
 
