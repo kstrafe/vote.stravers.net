fea7a4d7211bac6213cfb28db906b3f5b9ecdb24
Bourgond Aries <macocio@gmail.com>
2016-04-03 14:54:08 +0200

    Use a cookie middleware handler

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,62 @@
 #![feature(custom_attribute, custom_derive, plugin)]
 #![plugin(clippy, dotenv_macros, maud_macros)]
 extern crate chrono;
+extern crate cookie;
 extern crate dotenv;
 extern crate iron;
 extern crate maud;
+extern crate oven;
 extern crate postgres;
 extern crate rand;
 extern crate router;
 extern crate urlencoded;
 
 use iron::prelude::*;
+use iron::status;
+use oven::prelude::*;
 use postgres::{Connection, SslMode};
+use router::Router;
+
+fn handle(req: &mut Request) -> IronResult<Response> {
+	let cookie = req.get_cookie("hey");
+	println!("HEY!");
+	let mut resp = Response::new();
+	let mut nextval = 1i32;
+	if let Some(value) = cookie {
+		let val = &value.value;
+		if let Ok(val) = val.parse::<i32>() {
+			nextval += val;
+		}
+	}
+	println!("{}", nextval);
+	resp.set_cookie(cookie::Cookie::new(
+		"hey".into(), nextval.to_string()));
+	Ok(Response::with((
+		status::Ok,
+		"Hello world"
+	)))
+}
 
 fn main() {
-	use router::Router;
 
-	let conn = Connection::connect("postgresql://kefin@localhost/diesel_demo",
+	let conn = Connection::connect(
+		"postgresql://kefin@localhost/diesel_demo",
 		SslMode::None)
 		.unwrap();
+
 	for row in conn.query("select * from poll", &[])
-		.unwrap().iter() {
+	.unwrap().iter() {
 		let x: i64 = row.get(0);
 		println!("{}", x);
 	}
+
 	let router = Router::new();
+	let mut chain = iron::middleware::Chain::new(handle);
+	chain.link(oven::new(vec![]));
 
 	println!("Server started on :3000");
 
-	Iron::new(router)
+	Iron::new(chain)
 		.http("localhost:3000")
 		.unwrap();
 }
