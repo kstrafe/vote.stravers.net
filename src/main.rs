4c9b0d3036b1a463d7a3c9d4950c9dad2de1ffc2
Bourgond Aries <macocio@gmail.com>
2016-04-03 15:23:40 +0200

    Add get/post parsing

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,45 @@ extern crate rand;
 extern crate router;
 extern crate urlencoded;
 
+use iron::{BeforeMiddleware, AfterMiddleware, typemap};
 use iron::prelude::*;
 use iron::status;
 use oven::prelude::*;
 use postgres::{Connection, SslMode};
 use router::Router;
+use urlencoded::{QueryMap, UrlEncodedBody, UrlEncodedQuery};
+
+fn get<'a>(req: &'a mut Request) -> Option<&'a QueryMap> {
+	match req.get_ref::<UrlEncodedQuery>() {
+		Ok(hashmap) => Some(hashmap),
+		Err(_) => None,
+	}
+}
 
 fn handle(req: &mut Request) -> IronResult<Response> {
+	{
+		let conn = req.extensions.get::<DbCon>();
+		match conn {
+			Some(ref conn) => {
+				for row in conn.query("select * from poll", &[])
+				.unwrap().iter() {
+					let x: i64 = row.get(0);
+					println!("{}", x);
+				}
+			}
+			None => {
+				println!("Could not open connection!");
+			}
+		}
+	}
+
+	{
+		match get(req) {
+			Some(map) => println!("{:?}", map),
+			None => println!("Could not parse"),
+		}
+	}
+
 	let cookie = req.get_cookie("hey");
 	println!("HEY!");
 	let mut resp = Response::with((
@@ -37,22 +69,26 @@ fn handle(req: &mut Request) -> IronResult<Response> {
 	Ok(resp)
 }
 
-fn main() {
+struct DbCon;
 
-	let conn = Connection::connect(
-		"postgresql://kefin@localhost/diesel_demo",
-		SslMode::None)
-		.unwrap();
+impl typemap::Key for DbCon { type Value = Connection; }
 
-	for row in conn.query("select * from poll", &[])
-	.unwrap().iter() {
-		let x: i64 = row.get(0);
-		println!("{}", x);
+impl BeforeMiddleware for DbCon {
+	fn before(&self, req: &mut Request) -> IronResult<()> {
+		let conn = Connection::connect(
+			"postgresql://kefin@localhost/diesel_demo",
+			SslMode::None)
+			.unwrap();
+		req.extensions.insert::<DbCon>(conn);
+		Ok(())
 	}
+}
 
+fn main() {
 	let router = Router::new();
 	let mut chain = iron::middleware::Chain::new(handle);
 	chain.link(oven::new(vec![]));
+	chain.link_before(DbCon);
 
 	println!("Server started on :3000");
 
