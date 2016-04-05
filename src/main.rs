e9b7f1600dad713cd33b1599e54defb12748b030
Bourgond Aries <macocio@gmail.com>
2016-04-06 01:46:27 +0200

    Impl classify string and handle all errors

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,43 @@ impl AfterMiddleware for User {
 	}
 }
 
+#[derive(Clone, Debug)]
+enum CommandType {
+	Empty,
+	Get(String),
+	Set(String, String),
+}
+
+fn classify_string(command: &str) -> CommandType {
+	let parts = &mut command.splitn(2, '=');
+	match parts.clone().count() {
+		0 => CommandType::Empty,
+		1 => CommandType::Get(command.trim().into()),
+		2 => {
+			let error = "This can never happen because we check the amount of parts in the iterator. Defaulting to an Empty sequence";
+			let left = match parts.next() {
+				Some(left) => left.trim(),
+				None => {
+					error!("Left, {}", error);
+					return CommandType::Empty;
+				}
+			};
+			let right = match parts.next() {
+				Some(right) => right.trim(),
+				None => {
+					error!("Right, {}", error);
+					return CommandType::Empty;
+				}
+			};
+			CommandType::Set(left.into(), right.into())
+		}
+		_ => {
+			error!("Count not 0, 1, or 2. Indicating an splitn method error. Defaulting to no operation");
+			CommandType::Empty
+		}
+	}
+}
+
 fn two() {
 	use std::thread;
 	use std::sync::mpsc::channel;
@@ -52,18 +89,25 @@ fn two() {
 	let stdin = io::stdin();
 	thread::spawn(move || {
 		let mut stdin = stdin.lock();
-		loop {
+		'looper: loop {
 			let command = {
 				let mut command = String::new();
 				match stdin.read_line(&mut command) {
-					Ok(_) => debug!("Read stdin to string"),
-					Err(err) => error!("Unable to read std: {:?}", err),
+					Ok(bytes) => debug!("Read {} stdin bytes to string", bytes),
+					Err(err) => {
+						error!("Unable to read std: {:?},\nExiting interpreter", err);
+						break 'looper;
+					}
 				}
 				command
 			};
-			match send.send(command) {
-				Ok(()) => debug!("Message sent"),
-				Err(err) => error!("Unable to channel: {:?}", err),
+
+			{
+				let arg = classify_string(&command);
+				match send.send(arg) {
+					Ok(()) => debug!("Message sent"),
+					Err(err) => error!("Unable to send to channel: {:?}", err),
+				}
 			}
 		}
 	});
@@ -71,7 +115,7 @@ fn two() {
 	loop {
 		use std::sync::mpsc::TryRecvError;
 		match recv.try_recv() {
-			Ok(message) => trace!("Gotten message {}", message),
+			Ok(message) => trace!("Gotten message {:?}", message),
 			Err(TryRecvError::Empty) => {}
 			Err(TryRecvError::Disconnected) => error!("Sender dc'd"),
 		}
