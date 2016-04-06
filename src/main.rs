21abade2dff1b5a198d270748b2f134494c8e989
Bourgond Aries <macocio@gmail.com>
2016-04-06 02:42:02 +0200

    Add more macros for handling the requests

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ extern crate urlencoded;
 
 mod dbcon;
 mod controllers;
+mod live;
 mod response;
 mod setup_chain;
 
@@ -42,109 +43,6 @@ impl AfterMiddleware for User {
 	}
 }
 
-#[derive(Clone, Debug)]
-enum CommandType {
-	Empty,
-	Get(String),
-	Set(String, String),
-}
-
-fn classify_string(command: &str) -> CommandType {
-	let parts = &mut command.splitn(2, '=');
-	match parts.clone().count() {
-		0 => CommandType::Empty,
-		1 => CommandType::Get(command.trim().into()),
-		2 => {
-			let error = "This can never happen because we check the amount of parts in the iterator. Defaulting to an Empty sequence";
-			let left = match parts.next() {
-				Some(left) => left.trim(),
-				None => {
-					error!("Left, {}", error);
-					return CommandType::Empty;
-				}
-			};
-			let right = match parts.next() {
-				Some(right) => right.trim(),
-				None => {
-					error!("Right, {}", error);
-					return CommandType::Empty;
-				}
-			};
-			CommandType::Set(left.into(), right.into())
-		}
-		_ => {
-			error!("Count not 0, 1, or 2. Indicating an splitn method error. Defaulting to no operation");
-			CommandType::Empty
-		}
-	}
-}
-
-fn two() {
-	use std::thread;
-	use std::sync::mpsc::channel;
-	use std::io::BufRead;
-	use std::io;
-
-	let (send, recv) = channel();
-	let stdin = io::stdin();
-	thread::spawn(move || {
-		let mut stdin = stdin.lock();
-		'looper: loop {
-			let command = {
-				let mut command = String::new();
-				match stdin.read_line(&mut command) {
-					Ok(bytes) => debug!("Read {} stdin bytes to string", bytes),
-					Err(err) => {
-						error!("Unable to read std: {:?},\nExiting interpreter", err);
-						break 'looper;
-					}
-				}
-				command
-			};
-
-			{
-				let arg = classify_string(&command);
-				match send.send(arg) {
-					Ok(()) => debug!("Message sent"),
-					Err(err) => error!("Unable to send to channel: {:?}", err),
-				}
-			}
-		}
-	});
-
-	macro_rules! register_live {
-		($left:expr, $right:expr => $name:ident) => {
-			if $left == stringify!($name) {
-				$name = $right.into();
-			}
-			debug!("The string is {}", $name);
-		}
-	}
-
-	let mut fps = String::from("30");
-
-	loop {
-		use std::sync::mpsc::TryRecvError;
-		match recv.try_recv() {
-			Ok(message) => {
-				trace!("Gotten message {:?}", message);
-				match message {
-					CommandType::Empty => {}
-					CommandType::Get(string) => {
-					}
-					CommandType::Set(left, right) => {
-						register_live!(left, right => fps);
-					}
-				}
-			}
-			Err(TryRecvError::Empty) => {}
-			Err(TryRecvError::Disconnected) => error!("Sender dc'd"),
-		}
-		std::thread::sleep(std::time::Duration::new(3, 0));
-		debug!("fps: {}", fps);
-	}
-}
-
 fn main() {
 	match env_logger::init() {
 		Ok(()) => {}
@@ -154,7 +52,7 @@ fn main() {
 		}
 	}
 
-	two();
+	live::read_handler();
 
 	let handler = setup_chain::get_middleware();
 
